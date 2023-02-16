02f0c33a8fa2c56987b7334f13a5aa63ecc376e8
Michael <michael90@protonmail.com>
2023-02-16 20:57:21 +0100

    Update Target SDK to 33, set new minimum LND version to v0.15.0-beta

--- app/src/main/java/app/michaelwuensch/bitbanana/HomeActivity.java
+++ app/src/main/java/app/michaelwuensch/bitbanana/HomeActivity.java
@@ -460,8 +460,8 @@ public class HomeActivity extends BaseAppCompatActivity implements LifecycleObse
         App.getAppContext().connectionToLNDEstablished = true;
 
         // Warn the user if an old LND version is used.
-        if (Wallet.getInstance().getLNDVersion().compareTo(new Version("0.13.3")) < 0) {
-            new UserGuardian(this).securityOldLndVersion("v0.13.3-beta");
+        if (Wallet.getInstance().getLNDVersion().compareTo(new Version("0.15.0")) < 0) {
+            new UserGuardian(this).securityOldLndVersion("v0.15.0-beta");
         }
 
         // Fetch the transaction history
