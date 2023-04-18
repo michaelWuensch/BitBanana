8a2d4026bcbd728efd9a942f9dced2a7dfb1d4a4
hazrulnizam <47681422+hazrulnizam@users.noreply.github.com>
2023-04-19 01:57:25 +0800

    Fix incorrect date cut-off time in forwarding events view. (#15)

--- app/src/main/java/app/michaelwuensch/bitbanana/forwarding/listItems/DateItem.java
+++ app/src/main/java/app/michaelwuensch/bitbanana/forwarding/listItems/DateItem.java
@@ -1,5 +1,10 @@
 package app.michaelwuensch.bitbanana.forwarding.listItems;
 
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Locale;
+
 public class DateItem extends ForwardingListItem {
 
     public long mDate; // in milliseconds
@@ -12,7 +17,15 @@ public class DateItem extends ForwardingListItem {
         // To get the date line to show up at the correct position in the sorted list, we have to set its timestamp correctly.
         // We set it to 1 nanosecond before the day ends.
         // 86400000000000 = Nanoseconds of one day (60 * 60 * 24 * 1000 * 1000 * 1000)
-        mTimestampNS = (timestamp - (timestamp % 86400000000000L) + (86400000000000L - 1));
+        String tempDateText = new SimpleDateFormat("yyyy-MM-dd", Locale.US)
+                .format(mDate);
+        try {
+            Date d = new SimpleDateFormat("yyyy-MM-dd", Locale.US)
+                    .parse(tempDateText);
+            mTimestampNS = ((d.getTime() * 1000000L) + (86400000000000L - 1));
+        } catch (ParseException e) {
+            e.printStackTrace();
+        }
     }
 
     @Override
