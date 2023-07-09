51597c2716e0e523a564e1218542ff90bf542133
Michael <michael90@protonmail.com>
2023-07-09 10:58:39 +0200

    Create avatar images in background thread

--- app/src/main/java/app/michaelwuensch/bitbanana/contacts/ContactItemViewHolder.java
+++ app/src/main/java/app/michaelwuensch/bitbanana/contacts/ContactItemViewHolder.java
@@ -2,11 +2,16 @@ package app.michaelwuensch.bitbanana.contacts;
 
 import android.content.Context;
 import android.graphics.Bitmap;
+import android.os.Handler;
+import android.os.Looper;
 import android.view.View;
 import android.widget.TextView;
 
 import androidx.constraintlayout.utils.widget.ImageFilterView;
 import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 
 import app.michaelwuensch.bitbanana.R;
 import app.michaelwuensch.bitbanana.util.AvathorUtil;
@@ -33,10 +38,17 @@ public class ContactItemViewHolder extends RecyclerView.ViewHolder {
 
     public void bindContactItem(Contact contact) {
 
-        // Set User Avatar
-        Bitmap avatar = AvathorUtil.getAvathorWithCache(mContext, contact.getContactData(), 150);
-        mUserAvatar.setImageBitmap(avatar);
-
+        // Set User Avatar in background thread to ensure fluent scrolling
+        ExecutorService executor = Executors.newSingleThreadExecutor();
+        Handler handler = new Handler(Looper.getMainLooper());
+        executor.execute(() -> {
+            // Background
+            Bitmap avatar = AvathorUtil.getAvathorWithCache(mContext, contact.getContactData(), 150);
+            handler.post(() -> {
+                // UI Thread work
+                mUserAvatar.setImageBitmap(avatar);
+            });
+        });
 
         // Set Contact Name
         mContactName.setText(contact.getAlias());
