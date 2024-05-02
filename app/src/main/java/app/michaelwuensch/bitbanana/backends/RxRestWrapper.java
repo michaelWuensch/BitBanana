c5dbe3d3fa1ff7e50d0d9a858c8df89e663a6770
Michael Wünsch <michael90@protonmail.com>
2024-05-02 12:03:47 +0200

    Fix NetworkOnMainThread exception on LndHub calls

--- app/src/main/java/app/michaelwuensch/bitbanana/backends/RxRestWrapper.java
+++ app/src/main/java/app/michaelwuensch/bitbanana/backends/RxRestWrapper.java
@@ -12,6 +12,7 @@ import java.util.function.Function;
 
 import app.michaelwuensch.bitbanana.util.BBLog;
 import io.reactivex.rxjava3.core.Single;
+import io.reactivex.rxjava3.core.SingleEmitter;
 import okhttp3.Call;
 import okhttp3.Callback;
 import okhttp3.OkHttpClient;
@@ -19,7 +20,7 @@ import okhttp3.Request;
 import okhttp3.Response;
 
 public class RxRestWrapper {
-    private static boolean debug = false;
+    private static final boolean debug = false;
 
     private static final String LOG_TAG = RxRestWrapper.class.getSimpleName();
 
@@ -44,54 +45,69 @@ public class RxRestWrapper {
 
                 @Override
                 public void onResponse(@NonNull Call call, @NonNull Response response) {
-                    new Handler(Looper.getMainLooper()).post(new Runnable() {
-                        @Override
-                        public void run() {
-                            if (!emitter.isDisposed()) {
-                                if (response.isSuccessful() && response.body() != null) {
-                                    try {
-                                        String responseAsString = response.body().string();
-                                        if (debug)
-                                            BBLog.d(LOG_TAG, "Response for " + request.url() + ": " + responseAsString);
-                                        if (jsonResponseClass == null)
-                                            emitter.onSuccess(mapper.apply(null));
-                                        else {
-                                            T result = new Gson().fromJson(responseAsString, jsonResponseClass);
-                                            R mappedData = mapper.apply(result);
-                                            emitter.onSuccess(mappedData);
-                                        }
-                                    } catch (Exception e) {
-                                        if (e.getMessage() != null && !e.getMessage().isEmpty())
-                                            BBLog.w(LOG_TAG, e.getMessage());
-                                        emitter.onError(e);
-                                    } finally {
-                                        response.body().close();
-                                    }
-                                } else {
-                                    try {
-                                        String responseAsString = response.body().string();
-                                        RestErrorResponse errorResponse = new Gson().fromJson(responseAsString, RestErrorResponse.class);
-                                        if (errorResponse.getError() && errorResponse.getMessage() != null) {
-                                            BBLog.w(LOG_TAG, "Response failed: " + errorResponse.getMessage());
-                                            emitter.onError(new RuntimeException(errorResponse.getMessage()));
-                                        } else if (errorResponse.getDetail() != null) {
-                                            BBLog.w(LOG_TAG, "Response failed: " + errorResponse.getDetail());
-                                            emitter.onError(new RuntimeException(errorResponse.getMessage()));
-                                        } else {
-                                            BBLog.w(LOG_TAG, "Response failed: " + response.code());
-                                            BBLog.w(LOG_TAG, responseAsString);
-                                            emitter.onError(new RuntimeException("Response failed: " + response.code()));
-                                        }
-                                    } catch (Exception e) {
-                                        BBLog.w(LOG_TAG, "Response failed: " + response.code());
-                                        emitter.onError(new RuntimeException("Response failed: " + response.code()));
-                                    }
+                    if (!emitter.isDisposed()) {
+                        if (response.isSuccessful() && response.body() != null) {
+                            try {
+                                String responseAsString = response.body().string();
+                                if (debug)
+                                    BBLog.d(LOG_TAG, "Response for " + request.url() + ": " + responseAsString);
+                                if (jsonResponseClass == null)
+                                    onRxCallSuccess(emitter, mapper.apply(null));
+                                else {
+                                    T result = new Gson().fromJson(responseAsString, jsonResponseClass);
+                                    R mappedData = mapper.apply(result);
+                                    onRxCallSuccess(emitter, mappedData);
                                 }
+                            } catch (Exception e) {
+                                if (e.getMessage() != null && !e.getMessage().isEmpty())
+                                    BBLog.w(LOG_TAG, e.getMessage());
+                                onRxCallFailed(emitter, e);
+                            } finally {
+                                response.body().close();
+                            }
+                        } else {
+                            try {
+                                String responseAsString = response.body().string();
+                                RestErrorResponse errorResponse = new Gson().fromJson(responseAsString, RestErrorResponse.class);
+                                if (errorResponse.getError() && errorResponse.getMessage() != null) {
+                                    BBLog.w(LOG_TAG, "Response failed: " + errorResponse.getMessage());
+                                    onRxCallFailed(emitter, new RuntimeException(errorResponse.getMessage()));
+                                } else if (errorResponse.getDetail() != null) {
+                                    BBLog.w(LOG_TAG, "Response failed: " + errorResponse.getDetail());
+                                    onRxCallFailed(emitter, new RuntimeException(errorResponse.getDetail()));
+                                } else {
+                                    BBLog.w(LOG_TAG, "Response failed: " + response.code());
+                                    BBLog.w(LOG_TAG, responseAsString);
+                                    onRxCallFailed(emitter, new RuntimeException("Response failed: " + response.code()));
+                                }
+                            } catch (Exception e) {
+                                BBLog.w(LOG_TAG, "Response failed: " + response.code());
+                                onRxCallFailed(emitter, new RuntimeException("Response failed: " + response.code()));
                             }
                         }
-                    });
+                    }
                 }
             });
+        });
+    }
+
+    public static <T> void onRxCallFailed(SingleEmitter<T> emitter, Exception e) {
+        // Result needs to be executed on main thread
+        new Handler(Looper.getMainLooper()).post(new Runnable() {
+            @Override
+            public void run() {
+                emitter.onError(e);
+            }
+        });
+    }
+
+    public static <T> void onRxCallSuccess(SingleEmitter<T> emitter, T response) {
+        // Result needs to be executed on main thread
+        new Handler(Looper.getMainLooper()).post(new Runnable() {
+            @Override
+            public void run() {
+                emitter.onSuccess(response);
+            }
         });
     }
 }
